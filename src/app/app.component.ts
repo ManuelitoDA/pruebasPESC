0b06dfb533f7a80aad74d26584911758ed1b8db3
Ruul1999 <dominguezraul1999@gmail.com>
2022-10-16 21:22:16 -0500

    feat: se pueden añadir personas a la tabla y buscarlas por un buscador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,52 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pruebasPESC';
 
-  
+  inputSearch = '';
 
+  personasArray : Persona[] = [
+    { nombre: 'Raul', apellido: 'Dominguez', edad: 22, telefono: 8334496531 },
+    { nombre: 'Miguel', apellido: 'Del Angel', edad: 23, telefono: 8331383950 },
+    { nombre: 'Brandon', apellido: 'Vicencio', edad: 21, telefono: 8334536574 },
+    { nombre: 'Gerardo', apellido: 'Mendez', edad: 21, telefono: 8334536574 }
+  ];
 
+  personas : Persona[] = []
+
+  newName = '';
+  newLastName = '';
+  newAge = 0;
+  newPhoneNumber = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.buscarPersona();
+  }
+
+  addPerson(){
+    const newPerson: Persona = {
+      nombre: this.newName,
+      apellido: this.newLastName,
+      edad: this.newAge,
+      telefono: this.newPhoneNumber
+    }
+    this.personasArray.push(newPerson);
+  }
+
+  buscarPersona(){
+    setTimeout(() => {
+      if(this.inputSearch == ''){
+        this.personas = this.personasArray; 
+      }else{
+        let personasBuscadas: Persona[] = [];
+        this.personasArray.forEach((personaNombre) => {
+          if(personaNombre.nombre.trim().toLowerCase().includes(this.inputSearch.toLowerCase().trim())){
+            personasBuscadas.push(personaNombre);
+          }
+        });
+        this.personas = personasBuscadas;
+      }
+    }, 1)
+  }
 
 }
---
+++ src/app/interface.ts
@@ -0,0 +1,14 @@
+
+export interface Persona {
+    nombre: string,
+    apellido: string,
+    edad: number,
+    telefono: number
+}
+
+export interface Hermanos{ 
+    nombre: string,
+    apellido: string,
+    edad: number,
+    telefono: number
+}
